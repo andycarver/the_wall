0cb9d144202b2d7d508df6f9d81af3b2fac39440
andycarver <andrewmcarver@gmail.com>
2016-11-07 09:20:05 -0800

    added sql file

---
+++ thewall.sql
@@ -0,0 +1,85 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+
+-- -----------------------------------------------------
+-- Schema theWall
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema theWall
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `theWall` DEFAULT CHARACTER SET utf8 ;
+USE `theWall` ;
+
+-- -----------------------------------------------------
+-- Table `theWall`.`users`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `theWall`.`users` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `first_name` VARCHAR(255) NULL DEFAULT NULL,
+  `last_name` VARCHAR(255) NULL DEFAULT NULL,
+  `email` VARCHAR(255) NULL DEFAULT NULL,
+  `password` VARCHAR(255) NULL DEFAULT NULL,
+  `created_at` DATETIME NULL DEFAULT NULL,
+  `updated_at` DATETIME NULL DEFAULT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 6
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `theWall`.`messages`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `theWall`.`messages` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `message` TEXT NULL DEFAULT NULL,
+  `created_at` DATETIME NULL DEFAULT NULL,
+  `updated_at` DATETIME NULL DEFAULT NULL,
+  `user_id` INT(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_messages_users_idx` (`user_id` ASC),
+  CONSTRAINT `fk_messages_users`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `theWall`.`users` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+AUTO_INCREMENT = 5
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `theWall`.`comments`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `theWall`.`comments` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `comment` TEXT NULL DEFAULT NULL,
+  `created_at` DATETIME NULL DEFAULT NULL,
+  `updated_at` DATETIME NULL DEFAULT NULL,
+  `message_id` INT(11) NOT NULL,
+  `user_id` INT(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_comments_messages1_idx` (`message_id` ASC),
+  INDEX `fk_comments_users1_idx` (`user_id` ASC),
+  CONSTRAINT `fk_comments_messages1`
+    FOREIGN KEY (`message_id`)
+    REFERENCES `theWall`.`messages` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_comments_users1`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `theWall`.`users` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+AUTO_INCREMENT = 13
+DEFAULT CHARACTER SET = utf8;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
